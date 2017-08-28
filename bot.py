849e8e615b642a177794ac3ac007987e1b8348cc
dmarsh223 <danmarsh223@gmail.com>
2017-08-27 21:05:27 -0400

    redid api login added weather component tested facebook cross posting

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
-import random
-from io import BytesIO
-
-import requests
 import tweepy
-import time
-from PIL import Image
-from PIL import ImageFile
+import json
+from urllib.request import urlopen
 
 from secrets import *
 
 
-
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-
-# CONSUMER_KEY = 'Bw2Orl4Kb1lIYx1bxjc2dlJf5'
-# CONSUMER_SECRET = 'fdaYresY3zFCusOi4j7AUMcp0KXMIAjXbXySRjz51YipbufXt4'
-# ACCESS_TOKEN = '899423693453000704-r8N88iWYerA1oX1XP15RLQ6kUr3mAln'
-# ACCESS_TOKEN_SECRET = 'G8IIfR70aaGhRGuZp9xeQMQEiKiYiostgp76IoX5EMN6d'
-
+#authenticate to twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-# status = "Testing!"
+
+#gets weather from wunderground - under construction
+f = urlopen(WUNDERGROUND_LINK)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+location = parsed_json['location']['city']
+temp_f = parsed_json['current_observation']['temp_f']
+current_weather = ("Current temperature in %s is: %s" % (location, temp_f))
+print (current_weather)
+f.close()
+
+
+#update status test
+# status = current_weather
 # api.update_status(status=status)
 
+#prints out all tweets from timeline
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
