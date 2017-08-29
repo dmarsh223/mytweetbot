10d04314608ec015ca8bad32924e06e6dd9047d1
dmarsh223 <danmarsh223@gmail.com>
2017-08-28 20:42:45 -0400

    Added in check for alerts

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ api = tweepy.API(auth)
 
 
 #gets weather from wunderground - under construction
-f = urlopen(WUNDERGROUND_LINK)
+f = urlopen(WUNDERGROUND_WEATHER)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
@@ -22,11 +22,21 @@ print (current_weather)
 f.close()
 
 
+#checks for weather alerts
+alert = urlopen(WUNDERGROUND_ALERTS)
+alert_string = alert.read()
+parsed_alert_json = json.loads(alert_string)
+alert_type = parsed_alert_json['alerts'][0]['type']
+alert_description = parsed_alert_json['alerts'][0]['description'] #needs to check for number of alerts and iterate until none
+current_alert = ("The current alert type is %s and the description is %s" % (alert_type, alert_description))
+print(current_alert)
+alert.close
+
 #update status test
 # status = current_weather
 # api.update_status(status=status)
 
 #prints out all tweets from timeline
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
