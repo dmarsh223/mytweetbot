4ab81b3defe35387f450aea17dc0e766acec7af8
dmarsh223 <danmarsh223@gmail.com>
2017-09-01 20:53:55 -0400

    added block to check for number of active alerts

--- bot.py
+++ bot.py
@@ -25,19 +25,41 @@ f.close()
 alert = urlopen(WUNDERGROUND_ALERTS)
 alert_string = alert.read()
 parsed_alert_json = json.loads(alert_string)
-try:
-    alert_type = parsed_alert_json['alerts'][0]['type']
-    alert_description = parsed_alert_json['alerts'][0]['description'] #needs to check for number of alerts and iterate until none
-    current_alert = ("The current alert type is %s and the description is %s" % (alert_type, alert_description))
-    print(current_alert)
 
-   #sample for loop to iterate through different alerts
-    for item in parsed_alert_json["alerts"]:
-        print (item ["type"])
-    alert.close
-except:
-    print("There are no active alerts")
-    alert.close
+#this test shows that the number in the middle brackets corresponds to the alerts
+#so if there is more than 1 alert, this mumber can be used to iterate through them
+iterationtest = parsed_alert_json['alerts'][1]['type']
+print ("This is looking for the third alert, which has the type of %s" % (iterationtest))
+
+#this obtains the number of active weather alerts and stores is in number_of_alerts
+item_dict = json.loads(alert_string)
+number_of_alerts = len(item_dict['alerts'])
+print ('The number of active weather alerts is %s' % (number_of_alerts))
+
+
+
+
+
+
+
+
+
+#try not needed - block above can check for number of active alerts.
+# try:
+#     alert_type = parsed_alert_json['alerts'][0]['type']
+#     alert_description = parsed_alert_json['alerts'][0]['description']
+#     current_alert = ("The current alert type is %s and the description is %s" % (alert_type, alert_description))
+#     print(current_alert)
+#
+#
+#
+#    #sample for loop to iterate through different alerts
+#     for item in parsed_alert_json["alerts"]:
+#         print (item ["type"])
+#     alert.close
+# except:
+#     print("There are no active alerts")
+#     alert.close
 
 
 
