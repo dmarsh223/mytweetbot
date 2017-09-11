91eca1c6bc1e311b7ae56693839fb630035f66f3
Daniel Marsh <danmarsh223@gmail.com>
2017-09-10 21:14:44 -0400

    changed url for bhw weather when tweet is posted

--- bhwalerts.py
+++ bhwalerts.py
@@ -43,7 +43,7 @@ for i in range(number_of_alerts):
 
 
 # this is the status update that will be compared to previous tweets and eventually tweeted
-status_update = ("%s for Stafford Township expiring %s. Visit https://goo.gl/XwzVTp for"
+status_update = ("%s for Stafford Township expiring %s. Visit https://goo.gl/BK5kcp for"
                  " details." % (alert_description_list[0], alert_end_time[0]))
 
 
