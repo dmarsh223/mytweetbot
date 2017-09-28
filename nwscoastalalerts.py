f2b51e6a64fb7788569ed3388767d36c34b91f7a
dmarsh223 <danmarsh223@gmail.com>
2017-09-28 12:48:25 -0400

    added search to find element tag

--- nwscoastalalerts.py
+++ nwscoastalalerts.py
@@ -11,4 +11,14 @@ print (soup.prettify())
 
 
 
+summary = soup.summary.string[0]
+print (summary)
 
+summarytwo = soup.summary.string[1]
+print (summarytwo)
+
+
+#finds the summary elements and prints
+secondtable = soup.findAll('summary')[0]
+
+print (secondtable)
